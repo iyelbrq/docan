97dc747068ae7c4747e2af63bffa6678e4e73256
Muhammad Suhail <suhail@macbook.local>
2026-09-04 15:24:27 +0700

    update setting ip cloudflare

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,15 @@ Route::get('/syarat-ketentuan', [AuthController::class, 'terms'])->name('legal.t
 Route::get('/kebijakan-privasi', [AuthController::class, 'privacy'])->name('legal.privacy');
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'show'])->name('login');
-    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1')->name('login.submit');
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:30,1')->name('login.submit');
     Route::get('/lupa-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
-    Route::post('/lupa-password', [AuthController::class, 'sendResetLink'])->middleware('throttle:5,1')->name('password.email');
+    Route::post('/lupa-password', [AuthController::class, 'sendResetLink'])->middleware('throttle:15,1')->name('password.email');
     Route::get('/reset-password/{token}', [AuthController::class, 'showResetPassword'])->name('password.reset');
-    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->middleware('throttle:5,1')->name('password.update');
+    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->middleware('throttle:15,1')->name('password.update');
     Route::get('/daftar', [AuthController::class, 'showRegister'])->name('register');
-    Route::post('/daftar/cek-email', [AuthController::class, 'checkRegistrationEmail'])->middleware('throttle:20,1')->name('register.email.check');
-    Route::post('/daftar/cek-sf-code', [AuthController::class, 'checkRegistrationSalesForceCode'])->middleware('throttle:30,1')->name('register.sf-code.check');
-    Route::post('/daftar', [AuthController::class, 'register'])->middleware('throttle:10,1')->name('register.submit');
+    Route::post('/daftar/cek-email', [AuthController::class, 'checkRegistrationEmail'])->middleware('throttle:40,1')->name('register.email.check');
+    Route::post('/daftar/cek-sf-code', [AuthController::class, 'checkRegistrationSalesForceCode'])->middleware('throttle:40,1')->name('register.sf-code.check');
+    Route::post('/daftar', [AuthController::class, 'register'])->middleware('throttle:20,1')->name('register.submit');
 });
 Route::middleware(['auth', 'outlet.active'])->group(function () {
     Route::get('/', [PosController::class, 'index'])->name('pos');
